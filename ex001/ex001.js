8f2c157cf7cfc3e9ef00cf024d1cae00f740c94a
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-05-13 14:48:28 -0300

    Create ex001.js

---
+++ ex001/ex001.js
@@ -0,0 +1,11 @@
+var cachorro = {nome: "totó", raca: "Dalmata"}
+
+console.log(`Tipo: ${typeof cachorro}`);
+console.log(`Nome: ${cachorro.nome}`);
+console.log(`Raça: ${cachorro.raca}`);
+console.log("--------------------------")
+let meuNumero = "500";
+console.log(`Tipo: ${typeof meuNumero}`);
+meuNumero = 500;
+console.log(`Tipo: ${typeof meuNumero}`);
+console.log("--------------------------")
