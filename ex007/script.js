0f34179f0dad4293550d94f0bf097166f45e8fea
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-05-16 17:08:39 -0300

    ex007

--- ex007/script.js
+++ ex007/script.js
@@ -1,6 +1,18 @@
-let modal = document.getElementById("#meuModal");
+let modal = document.getElementById("meuModal");
+var closeBtn = document.getElementById("btnFechar")
+
 
 
 function abrirModal(){
-    
+    modal.style.display = "flex";
 }
+
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
