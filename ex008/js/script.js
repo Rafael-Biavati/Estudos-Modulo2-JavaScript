00a1684a2cfc9aa5e96ae43d352bc70444bac9fb
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-05-20 19:05:44 -0300

    ex008

--- ex008/js/script.js
+++ ex008/js/script.js
@@ -1,6 +1,24 @@
 
-{
-    
-}
-document.getElementById("texto").innerHTML = `${x}`
 
+const carro = {
+montadora:"ford", 
+modelo:"fiesta",
+ano:2015,
+placa:"ABC-1234", 
+ligar: function(){console.log("o carro esta ligado")},
+completo: function(){return (this.montadora + " " + this.modelo  + " - " + this.ano)}
+}; //objeto literal
+
+let camiseta = {tamanho:"",cor:"",preco:"", detalhar: function(){return ("Cor: " + this.cor) + " Preço: " + this.preco+ " Tamanho: " + this.tamanho} };
+
+camiseta.cor = "preta";
+camiseta.preco = 39.90;
+camiseta.tamanho = "P";
+
+
+console.log(carro.completo());
+console.log(carro.ligar());
+console.log(camiseta.preco);
+console.log(camiseta.tamanho);
+console.log(camiseta.cor);
+console.log(camiseta.detalhar());
