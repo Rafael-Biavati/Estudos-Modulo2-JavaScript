2e0b86e48bff3229ddecdafcf9fbc343b5edc8c2
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-05-25 00:24:56 -0300

    Update script.js

--- ex012/js/script.js
+++ ex012/js/script.js
@@ -6,11 +6,20 @@ class Carro{
         this.modelo = valor2;
         this.ano = valor3;
     }
+    andar(){
+        return this.modelo+ " esta andando..."
+    }
 }
 
 
 
 
 const uno = new Carro("fiat","uno",2001);
+const celta = new Carro("chevrolet","celta",2012);
 
 console.log(uno)
+console.log(uno.marca)
+console.log(uno.modelo)
+console.log(uno.ano)
+console.log(uno.andar())
+console.log(celta.andar())
