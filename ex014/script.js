64a9bca89f8cb7bd188c3412c1ff1ee70c6c83ec
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-05-27 16:28:29 -0300

    ex014

--- ex014/script.js
+++ ex014/script.js
@@ -1,1 +1,9 @@
-const carro
+const carro = {marca: "fiat",modelo: "uno", ano: 2001}
+
+
+
+let texto = JSON.stringify(carro)
+let objReconstruido = JSON.parse(texto)
+
+
+document.getElementById("area").innerHTML = objReconstruido.marca + "-" + objReconstruido.modelo + "-" + objReconstruido.ano + " |||| JSON = " + texto;
