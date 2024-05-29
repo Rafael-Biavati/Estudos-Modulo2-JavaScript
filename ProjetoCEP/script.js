e3ee2e357bd2d74e48ee3cfbe9946a4419c6a36f
Rafael-Biavati <161644504+Rafael-Biavati@users.noreply.github.com>
2024-05-29 16:48:24 -0300

    Update script.js

--- ProjetoCEP/script.js
+++ ProjetoCEP/script.js
@@ -2,24 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btnCep').addEventListener('click', buscarCep);
 });
 
-// Função principal que será chamada quando o botão for clicado
+
 const buscarCep = () => {
-    // Obtém o valor do campo de entrada de CEP e remove qualquer hífen
+    
     const cep = document.getElementById('cep').value.replace('-', '');
     
-    // Faz uma solicitação à API ViaCEP para obter informações sobre o CEP
+    //solicitação API ViaCEP para obter info sobre o CEP
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => response.json()) // Converte a resposta em formato JSON
+        .then(response => response.json()) // Converte em JSON
         .then(local => {
-            // Verifica se o CEP não tem erros
             if (!local.erro) {
                 // Cria uma string com o endereço formatado
                 let endereco = `${local.logradouro}, ${local.localidade}, ${local.uf}, Brasil`;
-                
-                // Chama a função para buscar as coordenadas do endereço
                 buscarCoordenadas(endereco);
-                
-                // Exibe o endereço no elemento com id "result"
+        
+                // Exibe o endereço
                 document.getElementById("result").innerHTML = 
                 "Cidade: " + local.localidade + "<br>" + 
                 "Estado: " + local.uf + "<br>" + 
@@ -27,36 +24,31 @@ const buscarCep = () => {
                 "Endereço: " + local.logradouro
 
             } else {
-                // Exibe um alerta se o CEP não for encontrado
                 alert('CEP não encontrado.');
             }
         })
         .catch(error => {
-            // Lida com erros na solicitação à API ViaCEP
             console.error('Erro ao buscar CEP:', error);
             alert('Erro ao buscar CEP. Por favor, tente novamente.');
             document.getElementById("mapa").style.display = "none";
             document.getElementById("cep").value = "";
             document.getElementById("cep").focus();
             document.getElementById("result").innerHTML = "";
-            
         });
 };
 
-// Função para buscar as coordenadas geográficas de um endereço usando a API Nominatim (OpenStreetMap)
+// Função para buscar as coordenadas usando a API nominatim
 const buscarCoordenadas = (endereco) => {
     fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${endereco}`)
-        .then(response => response.json()) // Converte a resposta em formato JSON
+        .then(response => response.json()) // Converte em JSON
         .then(data => {
-            // Verifica se a resposta contém dados
             if (data && data.length > 0) {
-                // Obtém latitude e longitude do primeiro resultado
+                // Obtém latitude e longitude do resultado
                 const { lat, lon } = data[0];
-                
-                // Chama a função para exibir o mapa com as coordenadas obtidas
+    
+                // Chama a função para exibir o mapa
                 exibirMapa(parseFloat(lat), parseFloat(lon));
             } else {
-                // Exibe um alerta se o endereço não for encontrado
                 alert('Endereço não encontrado.');
             }
         })
@@ -67,37 +59,26 @@ const buscarCoordenadas = (endereco) => {
         });
 };
 
-// Variável global para armazenar o mapa Leaflet
-let mapa;
 
-// Função para exibir o mapa com a localização específica
+let mapa;
 const exibirMapa = (latitude, longitude) => {
-    // Obtém o elemento HTML onde o mapa será exibido
     const mapaDiv = document.getElementById("mapa");
     
-    // Torna o div do mapa visível
-    mapaDiv.style.display = "flex";
+    //div mapa inicialmente com display: none
+    mapaDiv.style.display = "flex"; 
 
     // Remove o mapa anterior, se existir
     if (mapa) {
         mapa.remove();
     }
 
-    // Inicializa o mapa Leaflet com as coordenadas e um nível de zoom
 
-
-
-    //mapa = L.map('mapa').setView([latitude, longitude], 15);
+    // Inicializa o mapa com as coordenadas e um zoom(15)
     mapa = L.map('mapa').setView([latitude, longitude], 15);
 
-
-
-
-
-
-    // Adiciona os tiles (camadas) do OpenStreetMap ao mapa
+    // Adiciona tiles ao mapa gerando o mapa completo
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapa);
-    
-    // Adiciona um marcador no mapa na posição especificada
+
+    // Adiciona um marcador no mapa
     L.marker([latitude, longitude]).addTo(mapa);
 };
